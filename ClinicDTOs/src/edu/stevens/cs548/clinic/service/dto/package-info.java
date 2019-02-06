d9e6fc4090f0a1ea5c9145d9492b6d9bd966c397
MuxingLi <mli55@stevens.edu>
2019-02-06 17:54:23 -0500

    delete file

--- ClinicDTOs/src/edu/stevens/cs548/clinic/service/dto/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// 此文件是由 JavaTM Architecture for XML Binding (JAXB) 引用实现 v2.2.12-b141219.1637 生成的
-// 请访问 <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// 在重新编译源模式时, 对此文件的所有修改都将丢失。
-// 生成时间: 2017.10.29 时间 07:17:32 下午 EDT 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.example.org/clinic/schemas", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package edu.stevens.cs548.clinic.service.dto;
